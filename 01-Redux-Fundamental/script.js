1174ae9841a5fccd9ed0e4fb2b886520e6d0b3fd
Azizul <khanazizul1199@gmail.com>
2024-11-30 23:04:12 +0600

    Redux Fundamental Done

--- 01-Redux-Fundamental/script.js
+++ 01-Redux-Fundamental/script.js
@@ -20,10 +20,32 @@ let reduxState = {
 	age: 18,
 };
 
-// Immutable state change
-function reducer(state) {
-	return { ...state, count: state.count + 1 };
+// static  state change
+// function reducer(state) {
+// 	return { ...state, count: state.count + 1 };
+// }
+
+// reduxState = reducer(reduxState);
+// console.log(reduxState);
+
+// dynamically state change by action
+function reducer(state, action) {
+	if (!action?.type) throw new Error("Action type expected");
+	if (action.type === "count/increment") {
+		return { ...state, count: state.count + 1 };
+	} else if (action.type === "count/incrementBy") {
+		return { ...state, count: state.count + action.payload };
+	} else if (action.type === "count/decrement") {
+		return { ...state, count: state.count - 1 };
+	}
+	return state;
 }
 
-reduxState = reducer(reduxState);
+reduxState = reducer(reduxState, { type: "count/increment" });
+console.log(reduxState);
+reduxState = reducer(reduxState, { type: "count/increment" });
+console.log(reduxState);
+reduxState = reducer(reduxState, { type: "count/decrement" });
+console.log(reduxState);
+reduxState = reducer(reduxState, { type: "count/incrementBy", payload: 14 });
 console.log(reduxState);
