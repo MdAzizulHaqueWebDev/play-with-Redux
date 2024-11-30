3839f735096a63f6dfd8d2f360d145cc448e9e17
Azizul <khanazizul1199@gmail.com>
2024-11-30 22:35:48 +0600

    mutating vs immuting and references & strict equality

--- 01-Redux-Fundamental/script.js
+++ 01-Redux-Fundamental/script.js
@@ -8,9 +8,18 @@ let state = {
 // console.log(state);
 // state.count = state.count + 1
 // console.log(state);
+const prevState = state;
 
 function incrementCount() {
 	state.count = state.count + 1;
 }
 incrementCount();
 console.log(state);
+// after increment
+console.log(prevState === state); // true because this is references data type
+
+function incrementCountImmuting() {
+	state = { ...state, count: state.count + 1 };
+}
+incrementCountImmuting();
+console.log(prevState === state); // false because this is references data type
