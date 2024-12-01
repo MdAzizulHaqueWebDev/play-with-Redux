ede2725d0629a2ed6321ec45ce958c92882ec339
Azizul <khanazizul1199@gmail.com>
2024-12-01 20:30:48 +0600

    how does subscribe listener works more simple

--- 02-My-Redux/redux.js
+++ 02-My-Redux/redux.js
@@ -3,21 +3,22 @@ function createStore(reducer, ...options) {
 		throw new Error("Reducer function is required");
 
 	let state;
-	const listeners = [];
+	let listeners;
 	const store = {
 		getState() {
 			return state;
 		},
 		dispatch(action) {
-			console.log(listeners);
+			// console.log(listeners);
 			state = reducer(state, action);
 			// listeners.forEach((listener) => {
 			// 	listener();
 			// });
-			listeners[0]?.();
+			// listeners[0]?.();
+			listeners && listeners();
 		},
 		subscribe(listener) {
-			listeners.push(listener);
+			listeners = listener;
 		},
 	};
 	store.dispatch({ type: "@@Initial" });
