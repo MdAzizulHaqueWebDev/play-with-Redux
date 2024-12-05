0b579e48241e866de72fb13ab5963332a2a99f50
Azizul <khanazizul1199@gmail.com>
2024-12-05 22:37:26 +0600

    connect react-redux with react

--- 08_redux_with_react/App.js
+++ 08_redux_with_react/App.js
@@ -1,23 +1,21 @@
+import { useSelector } from "react-redux";
 import Product from "./components/Product";
-import { store } from "./store";
-import { productsList } from "./store/productsList";
-console.log(store);
+
 const App = () => {
+	const products = useSelector((state) => state.products);
 	return (
 		<>
 			<h1>Connect Redux with ReactJS App</h1>
 			<div className="products-container">
-				{store
-					.getState()
-					.products.map(({ id, title, rating, price, image }) => (
-						<Product
-							key={id}
-							title={title}
-							rating={rating}
-							price={price}
-							imageUrl={image}
-						/>
-					))}
+				{products.map(({ id, title, rating, price, image }) => (
+					<Product
+						key={id}
+						title={title}
+						rating={rating}
+						price={price}
+						imageUrl={image}
+					/>
+				))}
 			</div>
 		</>
 	);
--- 08_redux_with_react/main.js
+++ 08_redux_with_react/main.js
@@ -1,4 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./App.css";
-createRoot(document.querySelector("#root")).render(<App />);
+import { Provider } from "react-redux";
+import { store } from "./store/index";
+console.dir(Provider);
+createRoot(document.querySelector("#root")).render(
+	<>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</>,
+);
