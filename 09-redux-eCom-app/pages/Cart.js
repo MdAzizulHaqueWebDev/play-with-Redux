24c03919a48bb299f5ef18e5c583548182a15c9f
Azizul <khanazizul1199@gmail.com>
2024-12-06 21:02:53 +0600

    filter carts item from products list

--- 09-redux-eCom-app/pages/Cart.js
+++ 09-redux-eCom-app/pages/Cart.js
@@ -1,34 +1,41 @@
 import React from "react";
 import CartItem from "../components/CartItem";
+import { useSelector } from "react-redux";
 
 export default function Cart() {
-	const cartItems = [
-		{
-			id: 1,
-			title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-			imageUrl: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-			quantity: 1,
-			rating: 3.9,
-			price: 109.95,
-		},
-		{
-			id: 2,
-			title: "Mens Cotton Jacket",
-			imageUrl: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-			quantity: 1,
-			rating: 4.7,
-			price: 55.99,
-		},
-		{
-			id: 3,
-			title: "Mens Casual Slim Fit",
-			imageUrl: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-			quantity: 1,
-			rating: 2.1,
-			price: 15.99,
-		},
-	];
+	// const cartItems = [
+	// 	{
+	// 		id: 1,
+	// 		title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+	// 		imageUrl: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+	// 		quantity: 1,
+	// 		rating: 3.9,
+	// 		price: 109.95,
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		title: "Mens Cotton Jacket",
+	// 		imageUrl: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+	// 		quantity: 1,
+	// 		rating: 4.7,
+	// 		price: 55.99,
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		title: "Mens Casual Slim Fit",
+	// 		imageUrl: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+	// 		quantity: 1,
+	// 		rating: 2.1,
+	// 		price: 15.99,
+	// 	},
+	// ];
+	const { products, cartItems } = useSelector((state) => state);
+	const carts = products.filter((product) =>
+		cartItems.some((item) => item.productId === product.id),
+	);
+	console.log(carts);
 
+	console.log(cartItems);
 	return (
 		<div className="cart-container">
 			<h2>Items in Your Cart</h2>
