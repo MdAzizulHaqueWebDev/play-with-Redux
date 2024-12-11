cb5c8df82a633a502a69cb2e037f82e71debdf76
Azizul <khanazizul1199@gmail.com>
2024-12-11 16:33:54 +0600

    object is better then switch case

--- others/switchToObject.mjs
+++ others/switchToObject.mjs
@@ -1,19 +1,55 @@
-const curntDayIndx = new Date().getDay();
-const days = {
-	0: "saturday",
-	1: "sunday",
-	2: "monday",
-	3: "thuesday",
-	4: "wednesday",
-	5: "thursday",
-	6: "friday",
+// const curntDayIndx = new Date().getDay();
+// const days = {
+// 	0: "saturday",
+// 	1: "sunday",
+// 	2: "monday",
+// 	3: "thuesday",
+// 	4: "wednesday",
+// 	5: "thursday",
+// 	6: "friday",
+// };
+
+// const getDay =
+// 	(days, _default = "_default") =>
+// 	(dayIndex) =>
+// 		days[dayIndex] || _default;
+
+const getDay = () => {
+	return {
+		0: "saturday",
+		1: "sunday",
+		2: "monday",
+		3: "thuesday",
+		4: "wednesday",
+		5: "thursday",
+		6: "friday",
+	}[new Date().getDay()];
 };
+// console.log(getDay());
+// console.log(getDayName(curntDayIndx));
 
-const getDay =
-	(days, _default = "_default") =>
-	(dayIndex) =>
-		days[dayIndex] || _default;
+// function getEmoji(mood) {
+// 	let emoji;
+// 	if (mood === "happy") {
+// 		emoji = "😃";
+// 	} else if (mood === "sad") {
+// 		emoji = "Sad";
+// 	} else if (mood === "angry") {
+// 		emoji = "😡 Angry";
+// 	} else {
+// 		emoji = "😑";
+// 	}
+// 	return emoji;
+// }
 
-export const getDayName = getDay(days);
+function getEmoji(mood) {
+	return (
+		{
+			happy: "😁 happy",
+			sad: "😥 sad",
+			angry: "😡 Angry",
+		}[mood] || "😑 default"
+	);
+}
 
-console.log(getDayName(curntDayIndx));
+console.log("Getting emoji", getEmoji("a"));
