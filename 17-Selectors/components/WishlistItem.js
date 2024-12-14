a4fb12d5aea6a8da2c79dfac8485c3eed294e509
Azizul <khanazizul1199@gmail.com>
2024-12-14 09:51:48 +0600

    start redux selector

---
+++ 17-Selectors/components/WishlistItem.js
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux";
+
+export default function WishlistItem({
+	productId,
+	title,
+	rating,
+	price,
+	imageUrl,
+}) {
+	const dispatch = useDispatch();
+	console.log(productId, title, rating, price, imageUrl);
+	return (
+		<div className="cart-item-container">
+			<div className="cart-item">
+				<img src={imageUrl} alt={title} />
+				<div>
+					<h3>{title}</h3>
+					<p>{rating.rate} ★ ★ ★ ★</p>
+				</div>
+			</div>
+			<div className="item-price">${price}</div>
+			<div className="item-quantity"></div>
+		</div>
+	);
+}
