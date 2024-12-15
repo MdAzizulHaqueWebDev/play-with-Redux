080d8384f88aac397a36eb59925b6e05a1ccfa5a
Azizul <khanazizul1199@gmail.com>
2024-12-15 12:22:09 +0600

    fix: using createSelector

--- 18-custom-middleware-make-api-call/components/Header.js
+++ 18-custom-middleware-make-api-call/components/Header.js
@@ -1,54 +1,59 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import CartIcon from '../assets/cart-icon.svg'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import CartIcon from "../assets/cart-icon.svg";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  fetchProducts,
-  fetchProductsError,
-  updateAllProducts,
-} from '../store/slices/productsSlice'
-import { fetchCartItems, fetchCartItemsError, loadCartItems } from '../store/slices/cartSlice'
+	fetchProducts,
+	fetchProductsError,
+	updateAllProducts,
+} from "../store/slices/productsSlice";
+import {
+	fetchCartItems,
+	fetchCartItemsError,
+	loadCartItems,
+} from "../store/slices/cartSlice";
 
 export default function Header() {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchProducts())
-    fetch('https://fakestoreapi.com/products')
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch(updateAllProducts(data))
-      })
-      .catch(() => {
-        dispatch(fetchProductsError())
-      })
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchProducts());
+		fetch("http://localhost:3000/products")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				dispatch(updateAllProducts(data));
+			})
+			.catch(() => {
+				dispatch(fetchProductsError());
+			});
 
-    dispatch(fetchCartItems())
-    fetch('https://fakestoreapi.com/carts/5')
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch(loadCartItems(data))
-      })
-      .catch(() => {
-        dispatch(fetchCartItemsError())
-      })
-  }, [])
-  const cartItems = useSelector((state) => state.cartItems.list)
-  return (
-    <header>
-      <div className="header-contents">
-        <h1>
-          <Link to="/">Shopee</Link>
-        </h1>
-        <Link className="cart-icon" to="/cart">
-          <img src={CartIcon} alt="cart-icon" />
-          <div className="cart-items-count">
-            {cartItems.reduce(
-              (accumulator, currentItem) => accumulator + currentItem.quantity,
-              0
-            )}
-          </div>
-        </Link>
-      </div>
-    </header>
-  )
+		dispatch(fetchCartItems());
+		fetch("http://localhost:3000/carts/5")
+			.then((res) => res.json())
+			.then((data) => {
+				dispatch(loadCartItems(data));
+			})
+			.catch(() => {
+				dispatch(fetchCartItemsError());
+			});
+	}, []);
+	const cartItems = useSelector((state) => state.cartItems.list);
+	return (
+		<header>
+			<div className="header-contents">
+				<h1>
+					<Link to="/">Shopee</Link>
+				</h1>
+				<Link className="cart-icon" to="/cart">
+					<img src={CartIcon} alt="cart-icon" />
+					<div className="cart-items-count">
+						{cartItems.reduce(
+							(accumulator, currentItem) => accumulator + currentItem.quantity,
+							0,
+						)}
+					</div>
+				</Link>
+			</div>
+		</header>
+	);
 }
--- 18-custom-middleware-make-api-call/store/slices/cartSlice.js
+++ 18-custom-middleware-make-api-call/store/slices/cartSlice.js
@@ -1,57 +1,71 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSelector, createSlice } from "@reduxjs/toolkit";
 
 const findItemIndex = (state, action) =>
-  state.findIndex((cartItem) => cartItem.productId === action.payload.productId)
+	state.findIndex(
+		(cartItem) => cartItem.productId === action.payload.productId,
+	);
 
 const slice = createSlice({
-  name: 'cart',
-  initialState: {
-    loading: false,
-    list: [],
-    error: '',
-  },
-  reducers: {
-    fetchCartItems(state) {
-      state.loading = true
-    },
-    fetchCartItemsError(state, action) {
-      state.loading = false
-      state.error = action.payload || 'Something went wrong!'
-    },
-    loadCartItems(state, action) {
-      state.loading = false
-      state.list = action.payload.products
-    },
-    addCartItem(state, action) {
-      const existingItemIndex = findItemIndex(state.list, action)
-      if (existingItemIndex !== -1) state.list[existingItemIndex].quantity += 1
-      else state.list.push({ ...action.payload, quantity: 1 })
-    },
-    removeCartItem(state, action) {
-      const existingItemIndex = findItemIndex(state.list, action)
-      state.list.splice(existingItemIndex, 1)
-    },
-    increaseCartItemQuantity(state, action) {
-      const existingItemIndex = findItemIndex(state.list, action)
-      state.list[existingItemIndex].quantity += 1
-    },
-    decreaseCartItemQuantity(state, action) {
-      const existingItemIndex = findItemIndex(state.list, action)
-      state.list[existingItemIndex].quantity -= 1
-      if (state.list[existingItemIndex].quantity === 0)
-        state.list.splice(existingItemIndex, 1)
-    },
-  },
-})
+	name: "cart",
+	initialState: {
+		loading: false,
+		list: [],
+		error: "",
+	},
+	reducers: {
+		fetchCartItems(state) {
+			state.loading = true;
+		},
+		fetchCartItemsError(state, action) {
+			state.loading = false;
+			state.error = action.payload || "Something went wrong!";
+		},
+		loadCartItems(state, action) {
+			state.loading = false;
+			state.list = action.payload.products;
+		},
+		addCartItem(state, action) {
+			const existingItemIndex = findItemIndex(state.list, action);
+			if (existingItemIndex !== -1) state.list[existingItemIndex].quantity += 1;
+			else state.list.push({ ...action.payload, quantity: 1 });
+		},
+		removeCartItem(state, action) {
+			const existingItemIndex = findItemIndex(state.list, action);
+			state.list.splice(existingItemIndex, 1);
+		},
+		increaseCartItemQuantity(state, action) {
+			const existingItemIndex = findItemIndex(state.list, action);
+			state.list[existingItemIndex].quantity += 1;
+		},
+		decreaseCartItemQuantity(state, action) {
+			const existingItemIndex = findItemIndex(state.list, action);
+			state.list[existingItemIndex].quantity -= 1;
+			if (state.list[existingItemIndex].quantity === 0)
+				state.list.splice(existingItemIndex, 1);
+		},
+	},
+});
+
+const selectorFn = ({ products, cartItems }) => {
+	return cartItems.list
+		.map(({ productId, quantity }) => {
+			const cartProduct = products.list.find(
+				(product) => product.id === productId,
+			);
+			return { ...cartProduct, quantity };
+		})
+		.filter(({ title }) => title);
+};
+export const getCartItems = createSelector(selectorFn, (state) => state);
 
 export const {
-  fetchCartItemsError,
-  fetchCartItems,
-  loadCartItems,
-  addCartItem,
-  removeCartItem,
-  increaseCartItemQuantity,
-  decreaseCartItemQuantity,
-} = slice.actions
+	fetchCartItemsError,
+	fetchCartItems,
+	loadCartItems,
+	addCartItem,
+	removeCartItem,
+	increaseCartItemQuantity,
+	decreaseCartItemQuantity,
+} = slice.actions;
 
-export default slice.reducer
+export default slice.reducer;
