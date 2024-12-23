ce63db4c17173e2aba47c0bbbd36ff3b6f30f0dc
Azizul <khanazizul1199@gmail.com>
2024-12-23 12:39:36 +0600

    RTK Query

---
+++ 21-RTK-Query/src/RTK_Queries/apiSlice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+	baseQuery: fetchBaseQuery({
+		baseUrl: "http://localhost:3000",
+	}),
+	endpoints: (builder) => {
+		console.log(builder);
+		return {
+			getTasks: builder.query({ query: () => "/tasks" }),
+		};
+	},
+});
+
+export const { useGetTasksQuery } = api;
